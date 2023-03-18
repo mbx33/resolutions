d76c79f578927b10316d27ee4b3a2df27ded8a7c
Matt Baxter <mbx33@hotmail.com>
2023-03-17 20:16:20 -0600

    fixing api/update client

--- pages/api/update-response.js
+++ pages/api/update-response.js
@@ -1,23 +1,24 @@
 // create the handler to update the response in the database
 
-import { supabase } from '../../utils/supabaseClient';
+import { supabase } from '../../utils/supabase';
 
 export default async function handler(req, res) {
-	const { table, column, response, user } = req.body;
+	// const { table, column, response, user } = req.body;
 
-	console.log({
-		table,
-		column,
-		response,
-		user,
-	});
+	// console.log({
+	// 	table,
+	// 	column,
+	// 	response,
+	// 	user,
+	// });
 
-	try {
-		const { data, error } = await supabase.from(table).select('*').eq('id', user.id);
-		console.log(data);
-		res.status(200).json({ message: 'success' });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'error' });
-	}
+	// try {
+	// 	const { data, error } = await supabase.from(table).select('*').eq('id', user.id);
+	// 	console.log(data);
+	// 	res.status(200).json({ message: 'success' });
+	// } catch (error) {
+	// 	console.log(error);
+	// 	res.status(500).json({ message: 'error' });
+	// }
+	res.status(200).json({ message: 'success' });
 }
---
+++ utils/supabase.js
@@ -0,0 +1,12 @@
+// create a supabase client
+
+import { createClient } from '@supabase/supabase-js';
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(url, key);
+
+// export the supabase client
+
+export { supabase };
