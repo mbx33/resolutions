179968bb67d563e754a1e8d52df1318f0793a21c
Matt Baxter <mbx33@hotmail.com>
2023-03-20 17:26:48 -0600

    removed darkNav for layout

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -5,7 +5,7 @@ import Layout from '../../components/Layout';
 
 const AboutPage = () => {
 	return (
-		<Layout darkNav={false} title="About Page">
+		<Layout title="About Page">
 			<h1>About Page</h1>
 		</Layout>
 	);
